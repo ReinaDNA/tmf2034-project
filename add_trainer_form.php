823fdec5c2efc0afb2474213cf9607fb98af0e1e
demons929 <102964635+demons929@users.noreply.github.com>
2025-12-31 13:21:34 +0800

    CRUD for trainer
    can add, update, delete trainer

---
+++ add_trainer_form.php
@@ -0,0 +1,21 @@
+<html>
+  <body>
+    <form action="insert_trainer.php" method="POST">
+    Trainer ID: <input type="text" name="trainer_id" required><br><br>
+    First Name: <input type="text" name="fname" required><br><br>
+    Last Name: <input type="text" name="lname" required><br><br>
+    Contact: <input type="text" name="contact" required><br><br>
+    Email: <input type="email" name="email" required><br><br>
+    Gender: <input type="text" name="gender"><br><br>
+    Specialization: <input type="text" name="specialization"><br><br>
+    Certification: <input type="text" name="certification"><br><br>
+    Door: <input type="text" name="door"><br><br>
+    Street: <input type="text" name="street"><br><br>
+    Postcode: <input type="text" name="postcode"><br><br>
+    City: <input type="text" name="city"><br><br>
+    State: <input type="text" name="state"><br><br>
+
+    <input type="submit">
+    </form>
+  </body>
+</html>
---
+++ update_trainer_form.php
@@ -0,0 +1,28 @@
+<?php
+include 'connect.php';
+
+$sql = "SELECT t.Trainer_ID,
+    t.Trainer_FName,
+    t.Trainer_LName,
+    t.Trainer_Contact,
+    t.Trainer_Email,
+    t.Trainer_Gender,
+    t.Trainer_Specialization,
+    t.Trainer_Certification,
+    t.Door,
+    t.Street,
+    p.Postcode,
+    p.City,
+    p.State FROM Trainer t JOIN Postcode p ON t.Postcode = p.Postcode";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+		$id = $row["Trainer_ID"];
+        echo "<a href='update_trainer.php?id="."$id" . "'>" . "\t\t" . $row["Trainer_ID"]."\t\t".$row["Trainer_FName"]."\t\t".$row["Trainer_LName"]."\t\t".$row["Trainer_Contact"]."\t\t".$row["Trainer_Email"]."\t\t".$row["Trainer_Gender"]."\t\t".$row["Trainer_Specialization"]."\t\t".$row["Trainer_Certification"]."\t\t".$row["Door"]."\t\t".$row["Street"]."\t\t".$row["Postcode"]."\t\t".$row["City"]."\t\t".$row["State"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+?>
