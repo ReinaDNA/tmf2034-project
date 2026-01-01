5c7a7fa4d8d92b747425672a3b96b89a854356a2
demons929 <huanyey@gmail.com>
2026-01-01 14:37:27 +0800

    change UI align with member format

--- delete_trainer.php
+++ delete_trainer.php
@@ -1,34 +1,41 @@
+<?php   
+include 'trainer.html';
+?>
+
 <html>
+<head>
+    <title>Delete Trainer</title>
+</head>
 <body>
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-Trainer Details <br>
------------------ <br>
-<p>ID: <input type="text" name="tr_id"></p>
-<p><input type="submit" name="Delete" value="Delete"></p>
-</form>
+    <h1>Delete Trainer</h1>
+</body>
+
+<body>
+    <form action="delete_trainer.php" method="get">
+        Trainer ID to Delete: <input type="text" name="trainer_id" required><br><br>
+        <input type="submit" value="Delete Trainer">
+    </form>
 </body>
 </html>
 
 <?php
 include 'connect.php';
 
-if(isset($_POST['Delete'])){
-    $id = $_POST['tr_id'];
-    
-    // Delete query
-    $sql = "DELETE FROM trainer WHERE Trainer_ID = '$id'";
+if(isset($_GET['trainer_id'])) {
+    $trainer_id = $_GET['trainer_id'];
+
+    $sql = "DELETE FROM trainer WHERE Trainer_ID = '$trainer_id'";
     $result = mysqli_query($conn, $sql);
 
-    if($result){
-        if(mysqli_affected_rows($conn) > 0){
-            echo $id . " is deleted";
-        } else {
-            echo "No record found for ID: $id";
-        }
+    if($result) {   
+    if ($conn->query($sql) ==TRUE ) {
+        echo $trainer_id . " Trainer is deleted successfully";
     } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        echo "No record found for ID: " . $trainer_id;
+    }
+    } else {
+        echo "No trainer ID provided.";
     }
 }
-
 mysqli_close($conn);
 ?>
