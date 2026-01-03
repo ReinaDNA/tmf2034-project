1fb3ee67e4e71c8a8b6f175dbe896cf4a0a2aaf6
Dr3nzzz <darrenhhy@gmail.com>
2026-01-03 18:07:41 +0800

    Updated membership files with membership_fee

--- add_membership.php
+++ add_membership.php
@@ -1,5 +1,6 @@
 <?php   
 include 'membership.html';
+include '../connect.php';
 ?>
 
 <html>
@@ -12,7 +13,17 @@ include 'membership.html';
 
         Membership_ID: <input type="text" name = "Membership_ID" required><br><br>
 
-        Member_ID: <input type="text" name = "Member_ID" required><br><br>
+        Member_ID:
+        <select name="Member_ID" id="Member_ID" required>
+        <option value ="">Please Choose</option>
+        <?php
+        $sql = "SELECT Member_ID FROM member";
+        $result = mysqli_query($conn, $sql);
+            if ($result) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<option value='{$row['Member_ID']}'>{$row['Member_ID']}</option>";
+                }}?>
+        </select><br><br>
 
         Membership_Type: 
         <select name="Membership_Type" id="Membership_Type" required>
@@ -38,24 +49,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $member_id = $_POST['Member_ID'];
     $membership_type = $_POST['Membership_Type'];
     $membership_status = "Active";
-    $start_date = new DateTime($_POST['Start_Date']);
-    $expiry_date = clone $start_date;
+    $start_date = $_POST['Start_Date'];
+    $new_start_date = new DateTime($_POST['Start_Date']);
+    $expiry_date = clone $new_start_date;
 
 switch ($membership_type) {
     case "Monthly":
         $expiry_date->modify("+1 month");
+        $membership_fee = 100.00;
         break;
     case "Quarterly":
         $expiry_date->modify("+3 months");
+        $membership_fee = 300.00;
         break;
     case "Annually":
         $expiry_date->modify("+1 year");
+        $membership_fee = 1200.00;  
         break;
 }
+
     $sql = "INSERT INTO Membership
-            (Membership_ID, Member_ID, Membership_Type, Membership_Status, Start_Date, Expiry_Date)
+            (Membership_ID, Member_ID, Membership_Type, Membership_Status, Membership_Fee, Start_Date, Expiry_Date)
             VALUES
-            ('$membership_id', '$member_id', '$membership_type', '$membership_status', '".$start_date->format('Y-m-d')."', '".$expiry_date->format('Y-m-d')."')";
+            ('$membership_id', '$member_id', '$membership_type', '$membership_status', '$membership_fee', '".$new_start_date->format('Y-m-d')."', '".$expiry_date->format('Y-m-d')."')";
 
     if ($conn->query($sql) === TRUE) {
         echo "Membership added successfully";
--- update_membership_form.php
+++ update_membership_form.php
@@ -6,12 +6,13 @@ $membership_id = "";
 $member_id = "";
 $membership_type = "";
 $membership_status = "";
+$membership_fee = "";
 $start_date = "";
 $expiry_date = "";
 
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $sql = "SELECT * FROM membership WHERE Membership_ID = '$id'";
+if (isset($_GET['membership_id'])) {
+    $membership_id = $_GET['membership_id'];
+    $sql = "SELECT * FROM membership WHERE Membership_ID = '$membership_id'";
     $result = mysqli_query($conn, $sql);
 
     $row = mysqli_fetch_assoc($result);
@@ -21,6 +22,7 @@ if (isset($_GET['id'])) {
         $member_id = $row['Member_ID'];
         $membership_type = $row['Membership_Type'];
         $membership_status = $row['Membership_Status'];
+        $membership_fee = $row['Membership_Fee'];
         $start_date = $row['Start_Date'];
         $expiry_date = $row['Expiry_Date'];
 
@@ -34,8 +36,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $member_id = $_POST['Member_ID'];
     $membership_type = $_POST['Membership_Type'];
     $membership_status = $_POST['Membership_Status'];
-    $start_date = new DateTime($_POST['Start_Date']);
-    $expiry_date = clone $start_date;
+    $membership_fee = $_POST['Membership_Fee'];
+    $start_date = $_POST['Start_Date'];
+    $new_start_date = new DateTime($_POST['Start_Date']);
+    $expiry_date = clone $new_start_date;
 
     switch ($membership_type) {
     case "Monthly":
@@ -49,11 +53,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         break;
 }
 
+if ($membership_type == "Monthly") {
+    $membership_fee = 100.00;
+} elseif ($membership_type == "Quarterly") {
+    $membership_fee = 300.00;
+} elseif ($membership_type == "Annually") {
+    $membership_fee = 1200.00;
+}
+
     $sql = "UPDATE membership SET 
             Member_ID = '$member_id',
             Membership_Type = '$membership_type',
             Membership_Status = '$membership_status',
-            Start_Date = '" . $start_date->format('Y-m-d') . "',
+            Membership_Fee = '$membership_fee',
+            Start_Date = '" . $new_start_date->format('Y-m-d') . "',
             Expiry_Date = '" . $expiry_date->format('Y-m-d') . "'
             WHERE Membership_ID = '$membership_id'";
 
@@ -73,6 +86,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <p1>Update Membership Information Form For : <?php echo "(ID: " . $membership_id . ")"; ?></p1>
     <form action="update_membership_form.php" method="post">
         <input type="hidden" name="Membership_ID" value="<?php echo $membership_id; ?>">
+        <input type="hidden" name="Membership_Fee" value="<?php echo $membership_fee; ?>">
 
         Member ID: <input type="text" name = "Member_ID" value="<?php echo $member_id; ?>"><br><br>
 
@@ -81,7 +95,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <option value ="">Please Choose</option>
         <option value="Monthly" <?php if ($membership_type == 'Monthly') echo 'selected';?>>Monthly</option>
         <option value="Quarterly" <?php if ($membership_type == 'Quarterly') echo 'selected';?>>Quarterly</option>
-        <option value="Annually" <?php if ($membership_type == 'Annually') echo 'selected';?>>Annually</option><br><br> 
+        <option value="Annually" <?php if ($membership_type == 'Annually') echo 'selected';?>>Annual</option><br><br> 
         </select><br><br>
 
         Membership Status: 
